26e0a08a16b0abc41b1972f8d80f5870eaaba62c
MedyWSCW <159732892+MedyWSCW@users.noreply.github.com>
2024-09-05 10:16:03 +1200

    changes

--- main_menu.gd
+++ main_menu.gd
@@ -2,13 +2,13 @@ class_name MainMenu
 extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
-@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button  # Fixed extra space in the path
-@onready var start_level = preload("res://node_2d.tscn")
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var start_level = preload("res://scenes/node_2d.tscn") as PackedScene
 
-func _ready():
-	# Connect the pressed signal to the correct functions
-	start_button.button_down.connect("on_start_pressed")
-	exit_button.button_down.connect("button_exit_pressed")
+func _ready() -> void:
+	# Connect the "pressed" signal of both buttons
+	start_button.pressed.connect(on_start_pressed)
+
 
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
